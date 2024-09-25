325778d9ee5abc8bba1261965db06e99988d3114
C-Keenan <108506379+C-Keenan@users.noreply.github.com>
2024-09-25 14:57:59 -0400

    update to fix failing launch and to provide status if launch fails

--- main.go
+++ main.go
@@ -2,20 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 
+	"github.com/C-Keenan/DockerInteractor/controllers"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	//http.HandleFunc("/ctrlist", controllers.ListAllContainers)
+	http.HandleFunc("/ctrlist", controllers.ListAllContainers)
 	err := godotenv.Load()
 	if err != nil {
 		fmt.Println("Error loading .env file")
 		fmt.Println(err)
 	}
 	port := os.Getenv("PORT")
-	fmt.Printf("Server is running on localhost:%s\n", port)
-	http.ListenAndServe(port, nil)
+	fmt.Printf("Server is running on localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
